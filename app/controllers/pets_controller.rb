9cbb4e2412390a37a6610af5734647ff8f1a1660
Ryan Allen <rcallen89@gmail.com>
2020-01-30 15:46:14 -0700

    Create Pet Creation through Shelter Pet Index Create Spec for Pet Creation Create Routes for Pet Createion

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,19 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def new
+    @shelter_id = params[:id]
+  end
+
+  def create
+    pet = Pet.create(pet_params)
+    redirect_to "/shelters/#{pet.shelter_id}/pets"
+  end
+
+  private
+
+    def pet_params
+      params.permit(:image, :name, :description, :approximate_age, :sex, :status, :shelter_id)
+    end
 end
--- spec/features/shelters/shelter_pet_spec.rb
+++ spec/features/shelters/shelter_pet_spec.rb
@@ -26,3 +26,41 @@ RSpec.describe 'shelter pet page', type: :feature do
     end
   end
 end
+
+RSpec.describe 'shelter pet page', type: :feature do
+  context 'as a user' do
+    it 'has a create pet page and makes one' do
+      mike = Shelter.create(name: "Mike's Shelter",
+                            address: "1331 17th Street",
+                            city: 'Denver',
+                            state: 'CO',
+                            zip: '80202')
+
+      pet_2 = Pet.create(name: "Athena",
+                         approximate_age: "1 year old",
+                         sex: "F",
+                         description: 'butthead',
+                         image: 'https://image.shutterstock.com/image-photo/beagle-running-over-green-meadow-600w-1563583912.jpg',
+                         status: "available",
+                         shelter_id: mike.id)
+
+      visit "/shelters/#{mike.id}/pets"
+
+      click_on "Create Pet"
+
+      expect(current_path).to eq("/shelters/#{mike.id}/pets/new")
+
+      fill_in 'image', with: 'https://image.shutterstock.com/image-photo/beagle-running-over-green-meadow-600w-1563583912.jpg'
+      fill_in 'name', with: 'Ripley'
+      fill_in 'description', with: 'Whip It'
+      fill_in 'approximate_age', with: '5'
+      fill_in 'sex', with: 'M'
+
+      click_on "Create Pet"
+
+      expect(current_path).to eq("/shelters/#{mike.id}/pets")
+
+      expect(page).to have_content("Ripley")
+    end
+  end
+end
