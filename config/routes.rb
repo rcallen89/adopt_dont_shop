7de0d79dc330c38ce551fe4bfe2793cac0d2adb3
Ryan Allen <rcallen89@gmail.com>
2020-02-01 12:13:29 -0700

    Modify links for pet names

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ get '/shelters/:id/pets/new', to: 'pets#new'
 get '/shelters/:id/edit', to: 'shelters#edit'
 patch '/shelters/:id', to: 'shelters#update'
 delete '/shelters/:id', to: 'shelters#delete'
+
 get '/pets', to: 'pets#index'
 get '/pets/:id', to: 'pets#show'
 get '/pets/:id/edit', to: 'pets#edit'
