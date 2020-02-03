3cb4f82c8534c252c390f172e1edb317ad260525
Ryan Allen <rcallen89@gmail.com>
2020-02-02 18:08:26 -0700

    Modify Styling for Pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,8 @@
 class Pet < ApplicationRecord
   validates_presence_of :image, :name, :age, :description, :sex, :shelter_id, :status
   belongs_to :shelter
+
+  def gender
+    sex.split(//).first.capitalize
+  end
 end
